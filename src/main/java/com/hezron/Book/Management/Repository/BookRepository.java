fab90cb50ce3bc65d00de28cb1da4ea7aabe0ead
bettonhezron <hezronbet@gmail.com>
2025-02-17 12:21:10 +0300

    Updated book repository

--- src/main/java/com/hezron/Book/Management/Repository/BookRepository.java
+++ src/main/java/com/hezron/Book/Management/Repository/BookRepository.java
@@ -2,9 +2,18 @@ package com.hezron.Book.Management.Repository;
 
 
 import com.hezron.Book.Management.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface BookRepository extends JpaRepository<Book, String> {
+    Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+    Page<Book> findByAuthorContainingIgnoreCase(String author, Pageable pageable);
+    Page<Book> findByGenre(String genre, Pageable pageable);
+    List<Book> findByYear(Integer year);
+    boolean existsByIsbn(String isbn);
 
 }
